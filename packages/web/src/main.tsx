554871a0fb29a13f00c5c6c97333227aabde92a6
Amos Aidoo <aidooamos20@gmail.com>
2024-01-10 15:58:46 +0100

    fix: inside frag?

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -19,8 +19,10 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
+  <>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
     <Analytics />
-  </React.StrictMode>
+  </>
 )
