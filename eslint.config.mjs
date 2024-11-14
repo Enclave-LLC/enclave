6533c21568dc8e2921ad50349cf1b4e0b152f12f
Amos Aidoo <aidooamos20@gmail.com>
2024-11-14 19:15:44 +0100

    fix: update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+    plugins: {
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+        parserOptions: {
+            warnOnUnsupportedTypeScriptVersion: false,
+        },
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+    },
+}];
