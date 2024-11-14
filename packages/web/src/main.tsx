4ccd03469345affcad20ec106b9d906a0b6d54d7
Amos Aidoo <aidooamos20@gmail.com>
2024-11-14 19:15:05 +0100

    fix: disable appp routes

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -5,12 +5,12 @@ import "component-library/dist/style.css"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Waitlist from "./pages/Waitlist.tsx"
 import { Analytics } from "@vercel/analytics/react"
-import App from "./App.tsx"
-import Homepage from "./pages/Homepage.tsx"
-import Spaces from "./pages/Spaces.tsx"
-import ListSpace from "./pages/ListSpace.tsx"
+// import App from "./App.tsx"
+// import Homepage from "./pages/Homepage.tsx"
+// import Spaces from "./pages/Spaces.tsx"
+// import ListSpace from "./pages/ListSpace.tsx"
 import ErrorPage from "./pages/ErrorPage.tsx"
-import { AddSpace, Dashboard, Vendor } from "./pages/Vendor"
+// import { AddSpace, Dashboard, Vendor } from "./pages/Vendor"
 import { MapsProvider } from "./context/MapContext.tsx"
 import { Toaster } from "component-library"
 
@@ -19,45 +19,45 @@ const router = createBrowserRouter([
     path: "/",
     element: <Waitlist />,
     errorElement: <ErrorPage />
-  },
-  {
-    path: "/app",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />
-      },
-      {
-        path: "spaces",
-        element: <Spaces />
-      },
-      {
-        path: "list-space",
-        element: <ListSpace />
-      }
-    ]
-  },
-  {
-    path: "/app/vendor",
-    element: <Vendor />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />
-      }
-    ]
-  },
-  {
-    path: "/app/vendor/new-space",
-    element: <AddSpace />
-  },
-  {
-    path: "/app/customer",
-    element: <>Customer</>
   }
+  // {
+  //   path: "/app",
+  //   element: <App />,
+  //   errorElement: <ErrorPage />,
+  //   children: [
+  //     {
+  //       index: true,
+  //       element: <Homepage />
+  //     },
+  //     {
+  //       path: "spaces",
+  //       element: <Spaces />
+  //     },
+  //     {
+  //       path: "list-space",
+  //       element: <ListSpace />
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "/app/vendor",
+  //   element: <Vendor />,
+  //   errorElement: <ErrorPage />,
+  //   children: [
+  //     {
+  //       index: true,
+  //       element: <Dashboard />
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "/app/vendor/new-space",
+  //   element: <AddSpace />
+  // },
+  // {
+  //   path: "/app/customer",
+  //   element: <>Customer</>
+  // }
 ])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
